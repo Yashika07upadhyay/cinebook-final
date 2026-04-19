8d6465d0781ffcf03790477083e536ad7416b327
yashikau <yashika.u@inncircles.com>
2026-04-19 21:17:26 +0530

    fix: url issue fixed

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = process.env.CLIENT_URL;
+const SOCKET_URL = process.env.REACT_APP_API_URL;
 
 const socket = io(SOCKET_URL, {
   autoConnect: true,

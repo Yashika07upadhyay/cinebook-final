8603bb1dd16b47b053af182f4c731693252e29b9
yashikau <yashika.u@inncircles.com>
2026-04-19 21:14:36 +0530

    fix: base url fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = "https://cinebookfinal.onrender.com/";
 
 const api = axios.create({ baseURL: BASE_URL });
 
